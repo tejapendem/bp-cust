0cd338f46d35b4869dc6988fb2bf11c8b11b260c
Teja Pendem <teja.pendem@sap.com>
2026-06-12 11:55:55 +0530

    changed the mail dropdown

--- frontend/webapp/controller/ApprovalLevels.controller.js
+++ frontend/webapp/controller/ApprovalLevels.controller.js
@@ -8,6 +8,36 @@ sap.ui.define([
 
     return BaseController.extend("bp.cust.ui.controller.ApprovalLevels", {
 
+        formatEmailDisplay: function (sEmail) {
+            if (!sEmail) return "";
+            try {
+                var aEmails = JSON.parse(sEmail);
+                if (Array.isArray(aEmails) && aEmails.length > 0) {
+                    return aEmails.join(", ");
+                }
+            } catch (_) {}
+            if (typeof sEmail === "string" && sEmail.indexOf("@") > -1) return sEmail;
+            return "";
+        },
+
+        formatEmailDisplayVisible: function (sEmail) {
+            if (!sEmail) return false;
+            try {
+                var aEmails = JSON.parse(sEmail);
+                return Array.isArray(aEmails) && aEmails.length > 0;
+            } catch (_) {}
+            return typeof sEmail === "string" && sEmail.indexOf("@") > -1;
+        },
+
+        formatEmailPlaceholderVisible: function (sEmail) {
+            if (!sEmail) return true;
+            try {
+                var aEmails = JSON.parse(sEmail);
+                return !(Array.isArray(aEmails) && aEmails.length > 0);
+            } catch (_) {}
+            return !(typeof sEmail === "string" && sEmail.indexOf("@") > -1);
+        },
+
         onEmailSelectionChange: function (oEvent) {
             var oMulti = oEvent.getSource();
             var aItems = oMulti.getSelectedItems();
@@ -21,31 +51,29 @@ sap.ui.define([
             }
         },
 
-        _restoreAllCombos: function () {
-            var oTable = this.byId("approvalTable");
-            if (!oTable) return;
-            var aItems = oTable.getItems();
-            for (var i = 0; i < aItems.length; i++) {
-                var aCells = aItems[i].getCells();
-                var oMulti = null;
-                for (var j = 0; j < aCells.length; j++) {
-                    if (aCells[j] && aCells[j].getMetadata && aCells[j].getMetadata().getName() === "sap.m.MultiComboBox") {
-                        oMulti = aCells[j];
-                        break;
-                    }
+        _getRowControls: function (oRowItem) {
+            var aCells = oRowItem.getCells();
+            // Cell index 3 is the VBox containing display texts and MultiComboBox
+            var oVBox = aCells[3];
+            if (!oVBox) return null;
+            var aChildren = oVBox.getItems();
+            return {
+                displayText: aChildren[0],      // saved emails Text
+                placeholderText: aChildren[1],  // "No emails configured" Text
+                multiCombo: aChildren[2]        // MultiComboBox
+            };
+        },
+
+        _restoreComboForRow: function (oMulti, oCtx) {
+            if (!oMulti || !oCtx) return;
+            var sEmail = oCtx.getProperty("email");
+            if (!sEmail) return;
+            try {
+                var aEmails = JSON.parse(sEmail);
+                if (Array.isArray(aEmails) && aEmails.length > 0) {
+                    oMulti.setSelectedKeys(aEmails);
                 }
-                if (!oMulti) continue;
-                var oCtx = oMulti.getBindingContext();
-                if (!oCtx) continue;
-                var sEmail = oCtx.getProperty("email");
-                if (!sEmail) continue;
-                try {
-                    var aEmails = JSON.parse(sEmail);
-                    if (Array.isArray(aEmails) && aEmails.length > 0) {
-                        oMulti.setSelectedKeys(aEmails);
-                    }
-                } catch (_) {}
-            }
+            } catch (_) {}
         },
 
         onInit: function () {
@@ -59,10 +87,6 @@ sap.ui.define([
                         }.bind(this)
                     });
                 }
-            }
-            var oTable = this.byId("approvalTable");
-            if (oTable) {
-                oTable.attachEvent("updateFinished", this._restoreAllCombos, this);
             }
         },
 
@@ -92,6 +116,20 @@ sap.ui.define([
                 email: ""
             });
 
+            // After the new row renders, open its email combo automatically
+            setTimeout(function () {
+                var aItems = oTable.getItems();
+                var oNewItem = aItems[aItems.length - 1];
+                if (oNewItem) {
+                    var oControls = this._getRowControls(oNewItem);
+                    if (oControls) {
+                        oControls.displayText.setVisible(false);
+                        oControls.placeholderText.setVisible(false);
+                        oControls.multiCombo.setVisible(true);
+                    }
+                }
+            }.bind(this), 200);
+
             MessageToast.show("New level added (unsaved).");
         },
 
@@ -109,13 +147,17 @@ sap.ui.define([
         },
 
         onEditRowItem: function (oEvent) {
-            // Inputs are always editable inline; this focuses the first input in the row
             var oItem = oEvent.getSource().getParent().getParent();
-            var aCells = oItem.getCells();
-            if (aCells[1]) {
-                aCells[1].focus();
+            var oControls = this._getRowControls(oItem);
+            if (oControls) {
+                // Switch to edit mode: hide display texts, show MultiComboBox
+                oControls.displayText.setVisible(false);
+                oControls.placeholderText.setVisible(false);
+                oControls.multiCombo.setVisible(true);
+                // Pre-select already saved emails
+                this._restoreComboForRow(oControls.multiCombo, oControls.multiCombo.getBindingContext());
+                oControls.multiCombo.focus();
             }
-            MessageToast.show("Edit the fields inline, then click Save Changes.");
         },
 
         onSave: function () {
@@ -123,11 +165,31 @@ sap.ui.define([
             sap.ui.core.BusyIndicator.show(0);
             oModel.submitBatch("$auto").then(function () {
                 sap.ui.core.BusyIndicator.hide();
+                this._resetAllRowsToDisplayMode();
                 MessageBox.success("All changes saved to the backend.");
-            }).catch(function (oErr) {
+            }.bind(this)).catch(function (oErr) {
                 sap.ui.core.BusyIndicator.hide();
                 MessageBox.error("Failed to save: " + oErr.message);
             });
+        },
+
+        _resetAllRowsToDisplayMode: function () {
+            var oTable = this.byId("approvalTable");
+            if (!oTable) return;
+            oTable.getItems().forEach(function (oRowItem) {
+                var oControls = this._getRowControls(oRowItem);
+                if (!oControls) return;
+                var oCtx = oControls.multiCombo.getBindingContext();
+                var sEmail = oCtx ? oCtx.getProperty("email") : "";
+                var bHasEmails = false;
+                try {
+                    var aEmails = JSON.parse(sEmail);
+                    bHasEmails = Array.isArray(aEmails) && aEmails.length > 0;
+                } catch (_) {}
+                oControls.multiCombo.setVisible(false);
+                oControls.displayText.setVisible(bHasEmails);
+                oControls.placeholderText.setVisible(!bHasEmails);
+            }.bind(this));
         }
     });
 });
